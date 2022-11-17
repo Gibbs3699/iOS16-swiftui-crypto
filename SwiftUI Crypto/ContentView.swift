07904f77488e6af8eb56345d767b3e9b801ee5fb
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-11-17 23:34:41 +0700

    init project

---
+++ SwiftUI Crypto/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 17/11/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Crypto/SwiftUI_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CryptoApp.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 17/11/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
