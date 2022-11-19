8d5cf05fb244bb17d42aec1be0428de8e4947de1
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-11-19 21:15:57 +0700

    add color extensions

--- SwiftUI Crypto/ContentView.swift
+++ SwiftUI Crypto/ContentView.swift
@@ -10,17 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .foregroundColor(Color.theme.accent)
+            Text("Hello, world!")
+                .foregroundColor(Color.theme.green)
+            Text("Hello, world!")
+                .foregroundColor(Color.theme.red)
+            Text("Hello, world!")
+                .foregroundColor(Color.theme.secondaryText)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().colorScheme(.dark)
     }
 }
