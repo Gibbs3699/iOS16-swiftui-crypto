71d9786e9c68707d5514b4ec9629c4e7a5ca3995
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-11-20 23:14:56 +0700

    add header in home view to navigation bar

---
+++ SwiftUI Crypto/Core/Home/View/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 20/11/2565 BE.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Header")
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .navigationBarHidden(true)
+    }
+}
--- SwiftUI Crypto/SwiftUI_CryptoApp.swift
+++ SwiftUI Crypto/SwiftUI_CryptoApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftUI_CryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
