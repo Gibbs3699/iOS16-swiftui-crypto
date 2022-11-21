91bb4e2a88b1266504833bc40bb4cffcbeb41650
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-11-21 20:49:39 +0700

    integrate animate button to home view

--- SwiftUI Crypto/Core/Home/View/HomeView.swift
+++ SwiftUI Crypto/Core/Home/View/HomeView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showProfile: Bool = false
+    
     var body: some View {
         ZStack {
             Color.theme.background
                 .ignoresSafeArea()
             
             VStack {
-                Text("Header")
+                homeHeader
                 Spacer(minLength: 0)
             }
         }
@@ -25,5 +28,37 @@ struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
             .navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showProfile ? "plus" : "info")
+                .animation(.none, value: 0)
+                .background(
+                    CircleButtonAnimationView(animate: $showProfile)
+                )
+            
+            Spacer()
+
+            Text(showProfile ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none, value: 0)
+            
+            Spacer()
+            
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showProfile ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showProfile.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
     }
 }
