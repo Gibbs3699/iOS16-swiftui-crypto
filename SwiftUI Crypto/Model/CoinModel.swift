d2b06324c392f7e689d6344dd78433c480375a85
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-11-22 22:06:52 +0700

    add coin model

---
+++ SwiftUI Crypto/Model/CoinModel.swift
@@ -0,0 +1,98 @@
+//
+//  CoinModel.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 22/11/2565 BE.
+//
+
+import Foundation
+
+// CoinGecko API
+/*
+ https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
+ {
+   "id": "bitcoin",
+   "symbol": "btc",
+   "name": "Bitcoin",
+   "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+   "current_price": 16089.7,
+   "market_cap": 309281919184,
+   "market_cap_rank": 1,
+   "fully_diluted_valuation": 338026114717,
+   "total_volume": 29460234699,
+   "high_24h": 16234.57,
+   "low_24h": 15649.4,
+   "price_change_24h": -144.8682895359434,
+   "price_change_percentage_24h": -0.89234,
+   "market_cap_change_24h": -2224471257.1053467,
+   "market_cap_change_percentage_24h": -0.7141,
+   "circulating_supply": 19214256.0,
+   "total_supply": 21000000.0,
+   "max_supply": 21000000.0,
+   "ath": 69045,
+   "ath_change_percentage": -76.66331,
+   "ath_date": "2021-11-10T14:24:11.849Z",
+   "atl": 67.81,
+   "atl_change_percentage": 23661.9843,
+   "atl_date": "2013-07-06T00:00:00.000Z",
+   "roi": null,
+   "last_updated": "2022-11-22T14:53:14.132Z",
+   "sparkline_in_7d": {
+     "price": [
+       16806.84579045593,
+       16895.02524166953,
+     ]
+   },
+   "price_change_percentage_24h_in_currency": -0.8923444309576969
+ }
+ */
+
+struct CoinModel: Identifiable, Codable {
+    let id, symbol, name: String
+    let image: String
+    let currentPrice: Double
+    let marketCap, marketCapRank, fullyDilutedValuation, totalVolume: Double?
+    let high24H, low24H, priceChange24H, priceChangePercentage24H: Double?
+    let marketCapChange24H, marketCapChangePercentage24H: Double?
+    let circulatingSupply, totalSupply, maxSupply, ath: Double?
+    let athChangePercentage: Double?
+    let athDate: String?
+    let atl, atlChangePercentage: Double?
+    let atlDate: String?
+    let lastUpdated: String?
+    let sparklineIn7D: SparklineIn7D?
+    let priceChangePercentage24HInCurrency: Double?
+    let currentHoldings: Double?
+
+    enum CodingKeys: String, CodingKey {
+        case id, symbol, name, image
+        case currentPrice = "current_price"
+        case marketCap = "market_cap"
+        case marketCapRank = "market_cap_rank"
+        case fullyDilutedValuation = "fully_diluted_valuation"
+        case totalVolume = "total_volume"
+        case high24H = "high_24h"
+        case low24H = "low_24h"
+        case priceChange24H = "price_change_24h"
+        case priceChangePercentage24H = "price_change_percentage_24h"
+        case marketCapChange24H = "market_cap_change_24h"
+        case marketCapChangePercentage24H = "market_cap_change_percentage_24h"
+        case circulatingSupply = "circulating_supply"
+        case totalSupply = "total_supply"
+        case maxSupply = "max_supply"
+        case ath
+        case athChangePercentage = "ath_change_percentage"
+        case athDate = "ath_date"
+        case atl
+        case atlChangePercentage = "atl_change_percentage"
+        case atlDate = "atl_date"
+        case lastUpdated = "last_updated"
+        case sparklineIn7D = "sparkline_in_7d"
+        case priceChangePercentage24HInCurrency = "price_change_percentage_24h_in_currency"
+        case currentHoldings
+    }
+}
+
+struct SparklineIn7D: Codable {
+    let price: [Double]?
+}
