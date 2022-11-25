0cac6f36459ab3e1a7186a8bc582c7b19a997438
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-11-25 23:24:46 +0700

    add coin symbol to CoinRowView

--- SwiftUI Crypto/Core/Home/View/CoinRowView.swift
+++ SwiftUI Crypto/Core/Home/View/CoinRowView.swift
@@ -12,7 +12,30 @@ struct CoinRowView: View {
     let coin: CoinModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            
+            Circle()
+                .frame(width: 30, height: 30)
+            
+            Text("\(coin.symbol.uppercased())")
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+            
+            Spacer()
+            
+//            VStack {
+//                Text("\(coin.currentPrice.asCurrencyWith6Decimals())")
+//                    .bold()
+//                    .foregroundColor(Color.theme.accent)
+//                Text("\(coin.priceChangePercentage24H.asPercentString())")
+//                    .foregroundColor(coin.priceChangePercentage24H ?? 0.00 >= 0 ? Color.theme.green : Color.theme.red)
+//            }
+        }
     }
 }
 
