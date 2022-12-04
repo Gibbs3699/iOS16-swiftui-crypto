6e7f844413023767cf112170c8f7d692e4401f57
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-04 21:15:30 +0700

    add CoinDataService in HomeViewModel

--- SwiftUI Crypto/Core/Home/ViewModel/HomeViewModel.swift
+++ SwiftUI Crypto/Core/Home/ViewModel/HomeViewModel.swift
@@ -6,17 +6,27 @@
 //
 
 import Foundation
+import Combine
 
 class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    private var dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { 
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+
     }
 }
 
