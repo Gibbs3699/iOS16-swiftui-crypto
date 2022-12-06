60dddf6c52ad9dc05a46cf18824c68b106371501
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-06 22:48:39 +0700

    init new project

---
+++ SwiftUI Crypto#2/SwiftUI Crypto#2/SwiftUI Crypto#2/SwiftUI_Crypto_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Crypto_2App.swift
+//  SwiftUI Crypto#2
+//
+//  Created by TheGIZzz on 6/12/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Crypto_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
