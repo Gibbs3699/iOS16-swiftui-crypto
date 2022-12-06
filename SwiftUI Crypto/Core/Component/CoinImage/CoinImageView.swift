2db39424dc257056fac38fe6a9a11630da8c04de
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-06 22:42:50 +0700

    add CoinImageViewModel and integrate to CoinImageView

--- SwiftUI Crypto/Core/Component/CoinImage/CoinImageView.swift
+++ SwiftUI Crypto/Core/Component/CoinImage/CoinImageView.swift
@@ -7,24 +7,13 @@
 
 import SwiftUI
 
-class CoinImageViewModel: ObservableObject {
-    
-    @Published var image: UIImage? = nil
-    @Published var isLoading: Bool = false
-    
-    init() {
-        getImage()
-    }
-    
-    private func getImage() {
-        
-    }
-    
-}
-
 struct CoinImageView: View {
     
-    @StateObject var vm: CoinImageViewModel = CoinImageViewModel()
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
     
     var body: some View {
         ZStack {
@@ -44,7 +33,7 @@ struct CoinImageView: View {
 
 struct CoinImageView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinImageView()
+        CoinImageView(coin: dev.coin)
             .previewLayout(.sizeThatFits)
             .preferredColorScheme(.dark)
     }
---
+++ SwiftUI Crypto/Core/Home/ViewModel/CoinImageViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  CoinImageViewModel.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 6/12/2565 BE.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+    
+    private var coin: CoinModel
+    private var dataService: CoinImageService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        self.dataService = CoinImageService(coin: coin)
+        self.addSubscribers()
+        self.isLoading = false
+    }
+    
+    private func addSubscribers() {
+        dataService.$image
+            .sink { [weak self] (_) in
+                self?.isLoading = false
+            } receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+            }
+            .store(in: &cancellables)
+
+    }
+    
+}
