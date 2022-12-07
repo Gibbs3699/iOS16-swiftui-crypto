a5cb921b668eb3a60d8c18f1b9e46de3b43ac259
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-07 23:14:47 +0700

    add LocalFileManager class to save coin image in folder directory

---
+++ SwiftUI Crypto/Utilities/LocalFileManager.swift
@@ -0,0 +1,15 @@
+//
+//  LocalFileManager.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 7/12/2565 BE.
+//
+
+import Foundation
+import SwiftUI
+
+class LocalFileManager {
+    
+    static let instance = LocalFileManager()
+    private init() {}
+}
