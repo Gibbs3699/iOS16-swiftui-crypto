c4027c3405b2721fad16596a657ae41d30f52a2f
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-08 22:33:14 +0700

    create saveImage func in LocalFileManager

--- SwiftUI Crypto/Utilities/LocalFileManager.swift
+++ SwiftUI Crypto/Utilities/LocalFileManager.swift
@@ -12,4 +12,44 @@ class LocalFileManager {
     
     static let instance = LocalFileManager()
     private init() {}
+    
+    func saveImage(image: UIImage, imageName: String, folderName: String) {
+        createFolderIfNeeded(folderName: folderName)
+        
+        guard let data = image.pngData(),
+              let url = getURLForImage(imageName: imageName, folderName: folderName) else { return }
+        
+        do {
+            try data.write(to: url)
+        } catch let error {
+            print("Error saving image. ImageName: \(imageName). \(error)")
+        }
+        
+    }
+    
+    private func createFolderIfNeeded(folderName: String) {
+        guard let url = getURLForFolder(folderName: folderName) else { return }
+        
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch let error {
+                print("Error creating directory. FolderName: \(folderName). \(error)")
+            }
+        }
+    }
+    
+    private func getURLForFolder(folderName: String) -> URL? {
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return url.appendingPathComponent(folderName)
+    }
+    
+    private func getURLForImage(imageName: String, folderName: String) -> URL? {
+        guard let folderURL = getURLForFolder(folderName: folderName) else {
+            return nil
+        }
+        return folderURL.appendingPathComponent(imageName + ".png")
+    }
 }
