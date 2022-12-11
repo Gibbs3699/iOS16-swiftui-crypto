70e6e16defa1a0f0b222162bf112ae0ac7c27287
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-11 14:58:42 +0700

    implement SearchBarView

--- SwiftUI Crypto/Core/Component/SearchBar/SearchBarView.swift
+++ SwiftUI Crypto/Core/Component/SearchBar/SearchBarView.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 
 struct SearchBarView: View {
+    
+    @Binding var searchText: String
+    
     var body: some View {
         HStack {
             Image(systemName: "magnifyingglass")
+                .foregroundColor(searchText.isEmpty ? Color.theme.secondaryText: Color.theme.accent)
             
-            Text("Search by name or symbol... ")
+            TextField("Search by name or symbol... ", text: $searchText)
+                .foregroundColor(Color.theme.accent)
+                .overlay(
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 10)
+                        .foregroundColor(Color.theme.accent)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                        .onTapGesture {
+                            searchText = ""
+                        }
+                    ,alignment: .trailing
+                )
         }
+        .font(.headline)
+        .padding()
+        .background(
+           RoundedRectangle(cornerRadius: 25)
+            .fill(Color.theme.background)
+            .shadow(color: Color.theme.accent.opacity(0.25), radius: 10, x: 0, y: 0)
+        )
+        .padding()
     }
 }
 
 struct SearchBarView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchBarView()
+        Group {
+            SearchBarView(searchText: .constant("Fill"))
+                .previewLayout(.sizeThatFits)
+            
+            SearchBarView(searchText: .constant(""))
+                .previewLayout(.sizeThatFits)
+        }
     }
 }
