8e70130f6269412fba2a5bdb59f88ddad76c5e5a
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-13 23:22:46 +0700

    add StatisticModel for statistic view

---
+++ SwiftUI Crypto/Model/StatisticModel.swift
@@ -0,0 +1,21 @@
+//
+//  StatisticModel.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 13/12/2565 BE.
+//
+
+import Foundation
+
+struct StatisticModel: Identifiable {
+    let id = UUID().uuidString
+    let title: String
+    let value: String
+    let percentageChange: Double?
+    
+    init(title: String, value: String, percentageChange: Double? = nil) {
+        self.title = title
+        self.value = value
+        self.percentageChange = percentageChange
+    }
+}
