e98ea9c180ef871f290ea8f9095ffbffc6a1b3fe
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-14 22:56:31 +0700

    add StatisticView

---
+++ SwiftUI Crypto/Core/Component/StatisticView/StatisticView.swift
@@ -0,0 +1,48 @@
+//
+//  StatisticView.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 14/12/2565 BE.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    
+    let stat: StatisticModel
+    
+    var body: some View {
+        VStack {
+            Text(stat.title)
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+            Text(stat.value)
+                .font(.headline)
+                .foregroundColor(Color.theme.accent)
+            HStack {
+                Image(systemName: "triangle.fill")
+                    .font(.caption2)
+                    .rotationEffect(Angle(degrees: (stat.percentageChange ?? 0) >= 0 ? 0 : 180))
+                
+                Text(stat.percentageChange?.asPercentString() ?? "")
+                    .font(.caption)
+                    .bold()
+            }
+            .foregroundColor(stat.percentageChange ?? 0.0 >= 0 ? Color.theme.green : Color.theme.red)
+            .opacity(stat.percentageChange == nil ? 0.0 : 1.0)
+        }
+    }
+}
+
+struct StatisticView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            StatisticView(stat: dev.stat1)
+                .previewLayout(.sizeThatFits)
+            StatisticView(stat: dev.stat2)
+                .previewLayout(.sizeThatFits)
+            StatisticView(stat: dev.stat3)
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
