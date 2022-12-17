fe26c9b0417e6f830ead334378c61808acd8f451
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-17 22:41:14 +0700

    add color extension for Crypto#2

---
+++ SwiftUI Crypto#2/SwiftUI Crypto#2/SwiftUI Crypto#2/Extensions/Colors.swift
@@ -0,0 +1,23 @@
+//
+//  Colors.swift
+//  SwiftUI Crypto#2
+//
+//  Created by TheGIZzz on 17/12/2565 BE.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
