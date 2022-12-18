eab497660d1d332724d21ddbc334f50d33759b5e
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-18 23:10:23 +0700

    add MarketDataModel

---
+++ SwiftUI Crypto/Model/MarketDataModel.swift
@@ -0,0 +1,24 @@
+//
+//  MarketDataModel.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 18/12/2565 BE.
+//
+
+import Foundation
+
+struct GlobalData: Codable {
+    let data: MarketDataModel?
+}
+
+struct MarketDataModel: Codable {
+    let totalMarketCap, totalVolume, marketCapPercentage: [String: Double]
+    let marketCapChangePercentage24HUsd: Double
+
+    enum CodingKeys: String, CodingKey {
+        case totalMarketCap = "total_market_cap"
+        case totalVolume = "total_volume"
+        case marketCapPercentage = "market_cap_percentage"
+        case marketCapChangePercentage24HUsd = "market_cap_change_percentage_24h_usd"
+    }
+}
