eb9577b398e51f6d579c0f4e05c01295ca342ecc
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-18 23:18:00 +0700

    add marketCap, volumn and btcDominance func

--- SwiftUI Crypto/Model/MarketDataModel.swift
+++ SwiftUI Crypto/Model/MarketDataModel.swift
@@ -21,4 +21,25 @@ struct MarketDataModel: Codable {
         case marketCapPercentage = "market_cap_percentage"
         case marketCapChangePercentage24HUsd = "market_cap_change_percentage_24h_usd"
     }
+    
+    var marketCap: String {
+        if let item = totalMarketCap.first(where: { $0.key == "usd" }) {
+            return "\(item.value)"
+        }
+        return ""
+    }
+    
+    var volumn: String {
+        if let item = totalVolume.first(where: { $0.key == "usd" }) {
+            return "\(item.value)"
+        }
+        return ""
+    }
+    
+    var btcDominance: String {
+        if let item = marketCapPercentage.first(where: { $0.key == "btc" }) {
+            return item.value.asPercentString()
+        }
+        return ""
+    }
 }
