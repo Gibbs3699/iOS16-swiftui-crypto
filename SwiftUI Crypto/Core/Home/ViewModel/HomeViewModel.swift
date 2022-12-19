4accea78389ea5112e22cbcf86a996892d68683f
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-19 22:42:03 +0700

    add marketDataService subscriber to HomeViewModel

--- SwiftUI Crypto/Core/Home/ViewModel/HomeViewModel.swift
+++ SwiftUI Crypto/Core/Home/ViewModel/HomeViewModel.swift
@@ -22,7 +22,8 @@ class HomeViewModel: ObservableObject {
     
     @Published var searchText: String = ""
     
-    private var dataService = CoinDataService()
+    private var coinDataService = CoinDataService()
+    private var marketDataService = MarketDataService()
     private var cancellables = Set<AnyCancellable>()
     
     init() {
@@ -31,12 +32,39 @@ class HomeViewModel: ObservableObject {
     
     func addSubscribers() {
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
                 self?.portfolioCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+        
+        marketDataService.$marketData
+            .map { (marketDataModel) -> [StatisticModel] in
+                
+                var stats: [StatisticModel] = []
+                
+                guard let data = marketDataModel else {
+                    return stats
+                }
+                
+                let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+                let volumn = StatisticModel(title: "24 Volumn", value: data.volumn)
+                let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+                let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
+                
+                stats.append(contentsOf: [
+                    marketCap,
+                    volumn,
+                    btcDominance,
+                    portfolio
+                ])
+                return stats
+            }
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
             }
             .store(in: &cancellables)
 
