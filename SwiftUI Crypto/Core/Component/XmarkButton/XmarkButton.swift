f06ff63b2741d246d7bb596250c1fb5a177a7cb9
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-12-20 22:59:27 +0700

    add XmarkButton component

---
+++ SwiftUI Crypto/Core/Component/XmarkButton/XmarkButton.swift
@@ -0,0 +1,28 @@
+//
+//  XmarkButton.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 20/12/2565 BE.
+//
+
+import SwiftUI
+
+struct XmarkButton: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        } , label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+        })
+    }
+}
+
+struct XmarkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XmarkButton()
+    }
+}
