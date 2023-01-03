d206663504c8a01dcb1e8cd259f819f55351baf3
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-01-03 22:15:26 +0700

    init PortfolioDataService and add getPortfolio func

--- SwiftUI Crypto/Service/PortfolioDataService.swift
+++ SwiftUI Crypto/Service/PortfolioDataService.swift
@@ -6,7 +6,32 @@
 //
 
 import Foundation
+import CoreData
 
 class PortfolioDataService {
     
+    private let container: NSPersistentContainer
+    private let containerName: String = "PortfolioContainer"
+    private let entityName: String = "PortfolioEntity"
+    
+    @Published var savedEntities: [PortfolioEntity] = []
+    
+    init() {
+        container = NSPersistentContainer(name: containerName)
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                print("Error loading Core Data! \(error)")
+            }
+        }
+        self.getPortfolio()
+    }
+    
+    private func getPortfolio() {
+        let request = NSFetchRequest<PortfolioEntity>(entityName: entityName)
+        do {
+            savedEntities = try container.viewContext.fetch(request)
+        } catch let error {
+            print("Error fetching portfolio entities. \(error)")
+        }
+    }
 }
