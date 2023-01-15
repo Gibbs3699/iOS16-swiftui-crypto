9ad8098d03137d3f998c457beaa2b763d3abafcf
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-01-15 14:48:11 +0700

    add sortOption to coin columnTitles in HomeView

--- SwiftUI Crypto/Core/Home/View/HomeView.swift
+++ SwiftUI Crypto/Core/Home/View/HomeView.swift
@@ -113,6 +113,10 @@ extension HomeView {
     private var columnTitles: some View {
         HStack {
             Text("Coin")
+            Image(systemName: "chevron.down")
+                .opacity((vm.sortOption == .rank || vm.sortOption == .rankReversed) ? 1.0 : 0.0)
+                .rotationEffect(Angle(degrees: vm.sortOption == .rank ? 0 : 180))
+            
             Spacer()
             if showPortfolio {
                 Text("Holdings")
