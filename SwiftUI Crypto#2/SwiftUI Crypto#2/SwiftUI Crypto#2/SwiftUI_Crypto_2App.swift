fd6fb8ae47ed3d8e4096ec24bd2fdd1cb6320bbc
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-01-17 22:27:39 +0700

    add NavigationView to Crypto#2

--- SwiftUI Crypto#2/SwiftUI Crypto#2/SwiftUI Crypto#2/SwiftUI_Crypto_2App.swift
+++ SwiftUI Crypto#2/SwiftUI Crypto#2/SwiftUI Crypto#2/SwiftUI_Crypto_2App.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftUI_Crypto_2App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
