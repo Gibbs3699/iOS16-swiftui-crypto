5fa58abb8baf21e424f1a06a57cd89f763bd6baf
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-01-19 22:08:04 +0700

    add NavigationLink to allCoinList in HomeView

--- SwiftUI Crypto/Core/Home/View/HomeView.swift
+++ SwiftUI Crypto/Core/Home/View/HomeView.swift
@@ -93,8 +93,10 @@ extension HomeView {
     private var allCoinList: some View {
         List {
             ForEach(vm.allCoins) { coin in
-                CoinRowView(coin: coin, showHoldingColumns: false)
-                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                NavigationLink(destination: DetailView(coin: coin), label: {
+                    CoinRowView(coin: coin, showHoldingColumns: false)
+                        .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                })
             }
         }
         .listStyle(PlainListStyle())
