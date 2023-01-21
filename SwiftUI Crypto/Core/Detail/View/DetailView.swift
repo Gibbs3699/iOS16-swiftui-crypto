d9484481d829fc24f11ee5756983cbec1c451793
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-01-21 23:14:49 +0700

    binding coin to DetailView

--- SwiftUI Crypto/Core/Detail/View/DetailView.swift
+++ SwiftUI Crypto/Core/Detail/View/DetailView.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 struct DetailView: View {
     
-    let coin: CoinModel
+    @Binding var coin: CoinModel?
+    
+    init(coin: Binding<CoinModel?>) {
+        self._coin = coin
+        print("Initializing Detail View for \(coin.wrappedValue?.name)")
+    }
     
     var body: some View {
-        Text(coin.name)
+        Text(coin?.name ?? "")
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        DetailView(coin: .constant(dev.coin))
     }
 }
 
