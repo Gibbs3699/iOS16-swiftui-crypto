406da061744b472e571d90ad30f502ae79cec8ae
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-01-30 22:32:11 +0700

    add DetailViewModel

---
+++ SwiftUI Crypto/Core/Detail/ViewModel/DetailViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewModel.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 30/1/2566 BE.
+//
+
+import Foundation
+import Combine
+
+class DetailViewModel {
+    
+    private let coinDetailService: CoinDetailDataService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+        self.coinDetailService = CoinDetailDataService(coin: coin)
+        self.addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        coinDetailService.$coinDetails
+            .sink { (returnedCoinDetails) in
+                print("Recieved coin detail data")
+                print(returnedCoinDetails)
+            }
+            .store(in: &cancellables)
+    }
+    
+}
