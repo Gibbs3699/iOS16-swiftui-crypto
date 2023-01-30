e79249d32bf07d31108a7e661ae9f156f261cd39
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-01-30 22:27:03 +0700

    add CoinDetailDataService

---
+++ SwiftUI Crypto/Service/CoinDetailDataService.swift
@@ -0,0 +1,35 @@
+//
+//  CoinDetailDataService.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 30/1/2566 BE.
+//
+
+import Foundation
+import Combine
+
+class CoinDetailDataService {
+    
+    @Published var coinDetails: CoinDetailModel? = nil
+    
+    var coinDetailSubscription: AnyCancellable?
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinDetails()
+    }
+    
+    func getCoinDetails() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/\(coin.id)?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false") else { return }
+        
+        coinDetailSubscription = NetworkingManager.download(url: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .decode(type: CoinDetailModel.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoinDetails) in
+                self?.coinDetails = returnedCoinDetails
+                self?.coinDetailSubscription?.cancel()
+            })
+    }
+}
