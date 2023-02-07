3acb89bfcb66d97294cf408d09eaae36bcd5cb8c
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-07 14:29:51 +0700

    add chartDateLabel to chartView

--- SwiftUI Crypto/Core/Detail/View/ChartView.swift
+++ SwiftUI Crypto/Core/Detail/View/ChartView.swift
@@ -13,6 +13,8 @@ struct ChartView: View {
     private let minY: Double
     private let maxY: Double
     private let lineColor: Color
+    private let startingDate: Date
+    private let endingDate: Date
     
     init(coin: CoinModel) {
         data = coin.sparklineIn7D?.price ?? []
@@ -20,13 +22,22 @@ struct ChartView: View {
         maxY = data.max() ?? 0
         let priceChange = (data.last ?? 0) - (data.first ?? 0)
         lineColor = priceChange > 0 ? Color.theme.green : Color.theme.red
+        
+        endingDate = Date(coinGeckoString: coin.lastUpdated ?? "")
+        startingDate = endingDate.addingTimeInterval(-7*24*60*60)
     }
     
     var body: some View {
-        chartView
-            .frame(height: 200)
-            .background(chartBackground)
+        VStack {
+            chartView
+                .frame(height: 200)
+                .background(chartBackground)
             .overlay(chartYAxis, alignment: .leading)
+            
+            chartDateLabel
+        }
+        .font(.caption)
+        .foregroundColor(Color.theme.secondaryText)
     }
 }
 
@@ -70,6 +81,14 @@ extension ChartView {
             Text(((maxY + minY)/2).formattedWithAbbreviations())
             Spacer()
             Text(minY.formattedWithAbbreviations())
+        }
+    }
+    
+    private var chartDateLabel: some View {
+        HStack {
+            Text(startingDate.asShortDateString())
+            Spacer()
+            Text(endingDate.asShortDateString())
         }
     }
 }
