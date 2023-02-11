ca4b5a7c3608798da8a8b7fe190c97f3cc2860fa
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-11 18:59:52 +0700

    integrate setting view with HomeView

--- SwiftUI Crypto/Core/Home/View/HomeView.swift
+++ SwiftUI Crypto/Core/Home/View/HomeView.swift
@@ -12,9 +12,9 @@ struct HomeView: View {
     @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
     @State private var showPortfolioView: Bool = false
-    
     @State private var selectedCoin: CoinModel? = nil
     @State private var showDetailView: Bool = false
+    @State private var showSettingsView: Bool = false
     
     var body: some View {
         ZStack {
@@ -42,6 +42,9 @@ struct HomeView: View {
                 }
                 
                 Spacer(minLength: 0)
+            }
+            .sheet(isPresented: $showSettingsView) {
+                SettingView()
             }
         }
         .background(
@@ -73,6 +76,8 @@ extension HomeView {
                 .onTapGesture {
                     if showPortfolio {
                         showPortfolioView.toggle()
+                    } else {
+                        showSettingsView.toggle()
                     }
                 }
                 .background(
