54a5876818639fcf86b2405331eb83cf5984531e
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-11 16:02:17 +0700

    add personalSection to SettingView

---
+++ SwiftUI Crypto/Core/Setting/SettingView.swift
@@ -0,0 +1,61 @@
+//
+//  SettingView.swift
+//  SwiftUI Crypto
+//
+//  Created by TheGIZzz on 11/2/2566 BE.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    
+    let defaultURL = URL(string: "https://www.google.com")!
+    let youtubeURL = URL(string: "https://www.youtube.com")!
+    let githublURL = URL(string: "https://github.com/Gibbs3699/iOS16-swiftui-crypto")!
+    let githublProfileURL = URL(string: "https://github.com/Gibbs3699")!
+    
+    var body: some View {
+        NavigationView {
+            List() {
+                personalSection
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    XmarkButton()
+                }
+            }
+        }
+    }
+    
+}
+
+extension SettingView {
+    
+    private var personalSection: some View {
+        Section(header: Text("Personal")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("My Github 👩🏻‍💻", destination: githublURL)
+            Link("My Github Profile 📖", destination: githublProfileURL)
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
