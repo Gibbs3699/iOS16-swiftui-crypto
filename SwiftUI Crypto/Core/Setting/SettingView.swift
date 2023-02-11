bd0e52b8add3859ea238fac7571b5361a96a076e
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-11 16:13:31 +0700

    add coinGeckoSection to SettingView

--- SwiftUI Crypto/Core/Setting/SettingView.swift
+++ SwiftUI Crypto/Core/Setting/SettingView.swift
@@ -18,6 +18,7 @@ struct SettingView: View {
         NavigationView {
             List() {
                 personalSection
+                coinGeckoSection
             }
             .font(.headline)
             .accentColor(.blue)
@@ -52,6 +53,25 @@ extension SettingView {
             Link("My Github Profile 📖", destination: githublProfileURL)
         }
     }
+    
+    private var coinGeckoSection: some View {
+        Section(header: Text("CoinGecko")) {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit CoinGecko 😆", destination: githublURL)
+        }
+    }
+    
 }
 
 struct SettingView_Previews: PreviewProvider {
