009d1c5aab754c25bb21d02ad5df42cba9b5fa1d
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-13 17:26:30 +0700

    setup for iPad

--- SwiftUI Crypto/SwiftUI_CryptoApp.swift
+++ SwiftUI Crypto/SwiftUI_CryptoApp.swift
@@ -25,6 +25,7 @@ struct SwiftUI_CryptoApp: App {
                     HomeView()
                         .navigationBarHidden(true)
                 }
+                .navigationViewStyle(StackNavigationViewStyle())
                 .environmentObject(vm)
                 
                 ZStack {
